d8afd4048f750be4a79ea07553dca496db5c55f9
Bietood <gabriel.duarte69@etec.sp.gov.br>
2023-11-26 16:57:46 -0300

    Execício Resolvido

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int numero;
+
+Console.WriteLine();
+Console.Write("Digite um número e se quiser sair digite 0: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+while (numero < 0 || numero > 9)
+{
+    Console.Write("Só digite números entre 1 e 9 ou 0 se quiser sair: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+}
+
+if (numero == 0)
+{
+    Console.WriteLine("Tchau, volte sempre");
+}
+else
+{
+    Console.WriteLine("......");
+    Thread.Sleep(1500);
+    Console.WriteLine($"O número selecionado foi {numero}");
+}
